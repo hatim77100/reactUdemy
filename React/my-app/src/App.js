929d3b718afb41f843264200544149a1b03ca3e5
hatim77100 <najoua-hatim@hotmail.fr>
2022-09-29 22:44:07 +0200

    first commit

---
+++ React/my-app/src/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import "./App.css";
+import "../node_modules/bulma/css/bulma.min.css";
+import Header from "./Components/Header/Header";
+// import { useState } from "react";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/my-app/src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+    <nav className='navbar has-text-centered has-background-primary' role="navigation" aria-label='main navigation'>
+        <h2 className="is-size-4 has-text-white has-text-centered" style={{lineHeight:'52px'}}>
+            App to Do Liste
+        </h2>
+
+    </nav>
+    </div>
+  )
+}
