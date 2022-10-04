0f75c391c77d4fb781df3e8d1a43525731b28fd5
hatim77100 <najoua-hatim@hotmail.fr>
2022-10-04 21:12:17 +0200

    update app

---
+++ React-46h/2-Create-react-App/cars-soft/component/Cars.js
@@ -0,0 +1,18 @@
+const Car = ({color, children, year}) => {
+
+    const colorInfo = color ? <p>Couleur: {color}</p> : <p>Couleur: "Néant"</p>;
+
+    if (children) {
+        return (
+            <div style={ {backgroundColor: 'pink', width: '400px', padding: '10px', margin: '5px auto'} }>
+                <p>Marque: { children }</p>
+                <p>Age: { year }</p>
+                {colorInfo}
+            </div>
+        )
+    }
+
+    // return null // ce return est Facultatif
+}
+
+export default Car
---
+++ React-46h/2-Create-react-App/cars-soft/component/Mycars.js
@@ -0,0 +1,63 @@
+import { Component } from "react";
+import Car from "./Cars";
+
+class Mycars extends Component {
+  state = {
+    voiture: [
+      { name: "Ford", color: "red", year: 2022 },
+      { name: "Mercedes", color: "black", year: 2021},
+      { name: "Peugeot", color: "green", year: 2018 },
+    ],
+    titre: "Mon catalogue Voitures",
+  };
+
+  addtenYears = () => {
+    const updatedState = this.state.voiture.map((param) => {
+      return (param.year -= 10);
+    });
+    this.setState({
+      updatedState,
+    });
+  };
+
+  getAge = (year) => {
+    const now = new Date().getFullYear();
+    const age = now - year
+    
+    // ans, an,
+    let frenchYearStr = "";
+    if(age === 1) {
+        frenchYearStr = "an";
+    } else if (age > 1) {
+        frenchYearStr = "ans";
+    }
+    return `${age} ${frenchYearStr}`
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.titre}</h1>
+
+        <button onClick={this.addtenYears}>+ 10 ans</button>
+
+        {
+        this.state.voiture.map(({name, color, year}, index) => {
+          return (
+            <div key={index}>
+            <Car
+              color={color}
+              year= {this.getAge(year)}
+              >
+              {name}
+              </Car>
+            </div>
+          )
+        })
+        }
+      </div>
+    );
+  }
+}
+
+export default Mycars;
