8409a1436124813777e0b47677a97c5a0e08eecc
hatim77100 <najoua-hatim@hotmail.fr>
2022-10-13 22:58:56 +0200

    life Cycle

--- lifeCycle/methodes/src/App.js
+++ lifeCycle/methodes/src/App.js
@@ -3,11 +3,27 @@ import LifeCycle from './LifeCycle';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    display: true,
+  }
+
+  effacerOuAfficher = () => {
+    this.state.display ? (this.setState({ display: false})) : (this.setState({ display: true})) ;
+  }
+
   render () {
+
+    const showComponent = this.state.display ? (<LifeCycle name="Toto 2"/>) : (<div></div>);
 
     return (
       <div className="App">
-        <LifeCycle/>
+        
+      {
+        showComponent
+      }
+
+        <button onClick={this.effacerOuAfficher}>cliquer ici</button>
       </div>
     );
   }
--- lifeCycle/methodes/src/LifeCycle.js
+++ lifeCycle/methodes/src/LifeCycle.js
@@ -1,5 +1,5 @@
 import {Component} from 'react';
-import ChildComponent from './ChildComponent';
+// import ChildComponent from './ChildComponent';
 
 class LifeCycle extends Component {
 
@@ -15,18 +15,33 @@ class LifeCycle extends Component {
 
     componentDidMount() {
       console.log(`Etape ${this.state.step} : je suis dans componentDidMount()`);
+      this.setState({
+        name: this.props.name,
+        step: this.state.step + 1
+      })
+      console.log(`Etape ${this.state.step} : setState() a changé le state dans le componentDidMount()`);
+    };
+
+    componentDidUpdate(prevProps, prevState) {
+      console.log(`Etape ${this.state.step} : je suis dans le componentDidUpdate()`);
+      console.log(prevState);
+      console.log(this.state);
     }
+
+    componentWillUnmount() {
+      console.log(`je suis dans le componentWillUnmount()`);
+     }
 
   render () {
 
     console.log(`Etape ${this.state.step} : je suis dans le RENDER5()`);
 
     return (
-      <div className="App">
+      <div className="borderBox">
         {console.log(`Etape ${this.state.step} : Mise à jour du DOM`)} 
         <p>Chargement: {this.state.step}</p>
         <p>Nom: {this.state.name}</p>
-        <ChildComponent/>
+ 
       </div>
     );
   }
