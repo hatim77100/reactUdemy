f51c2b3b466fa12b6d61376bb3e2f36eb86e3005
hatim77100 <najoua-hatim@hotmail.fr>
2022-11-07 20:55:47 +0100

    update useReducer

--- 1-lesHooks/5-useReducer/test/src/App.js
+++ 1-lesHooks/5-useReducer/test/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Count from './components/Count';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- 1-lesHooks/5-useReducer/test/src/components/Count.js
+++ 1-lesHooks/5-useReducer/test/src/components/Count.js
@@ -1,29 +1,78 @@
-import React, {useReducer} from "react";
+import React, { useReducer } from "react";
 
-const initialState = 0;
+const initialState = {
+  countOne : 0,
+  countTwo : 0
+};
+
 const reducer = (state, action) => {
- switch(action) {
-    case 'increment' : return parseInt(state +1);
-    break;
-    case 'decrement' : return parseInt(state - 1);
-    break;
-    case 'réanitialisé' : return initialState;
-    break;
-    default : return state;
- }
-}
+  switch (action.type) {
+    case "increment":
+      return {...state, countOne: state.countOne + action.value};
+      break;
+    case "decrement":
+      return {...state, countOne: state.countOne - action.value};
+      break;
+    case "increment2":
+      return {...state, countTwo: state.countTwo + action.value};
+      break;
+    case "decrement2":
+      return {...state, countTwo: state.countTwo - action.value};
+      break;
+    case "réanitialisé":
+      return initialState;
+      break;
+    default:
+      return state;
+  }
+};
 
 function Count() {
+  const [count, dispatch] = useReducer(reducer, initialState);
 
-
-    const[count, dispatch] = useReducer(reducer, initialState);
-
-  return <div>
-    <h1>{count}</h1>
-    <button onClick={() => dispatch('increment')}>+</button>
-    <button onClick={() => dispatch( 'decrement')}>-</button>
-    <button onClick={() => dispatch( 'réanitialisé')}>0</button>
-  </div>;
+  return (
+    <div className="row">
+      <div className="col-6">
+        <p>{count.countOne}</p>
+        <button
+          className="btn btn-success m-3"
+          onClick={() => dispatch({type : "increment", value : 1})}
+        >
+          +
+        </button>
+        <button
+          className="btn btn-danger m-3"
+          onClick={() => dispatch({type : "decrement", value : 1})}
+        >
+          -
+        </button>
+      </div>
+      <div className="col-6">
+        <p>{count.countTwo}</p>
+        <button
+          className="btn btn-success m-3"
+          onClick={() => dispatch({type : "increment2", value : 10})}
+        >
+          +
+        </button>
+        <button
+          className="btn btn-danger m-3"
+          onClick={() => dispatch({type : "decrement2", value : 10})}
+        >
+          -
+        </button>
+ 
+      </div>
+      <div className="col">
+      <button
+          className="btn btn-primary m-3"
+          onClick={() => dispatch({type : "réanitialisé"})}
+        >
+          0
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Count;
